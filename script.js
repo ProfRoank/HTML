2bc0d0916df8a34210563549392b08461a8397e0
ProfRonak <ronakjoshi8299@gmail.com>
2025-12-31 10:47:03 +0530

    script file

---
+++ script.js
@@ -0,0 +1,51 @@
+// 6.Calculate the average of numbers from 1 to N using a while loop.
+// let n=1256, sum=0,i=1
+// while(i<=n){
+//     sum+=i
+//     i++
+// }
+// console.log("The Sum is:", sum)
+// console.log("The Avrage is:", sum/n)
+// 7.Print all multiples of 7 between 1 and 100 using a while loop.
+// let i=1
+// while(i<=100){
+//     if(i%7==0){
+//         console.log(i)
+//     }
+//     i++
+// }
+// 8.Check whether a number is a palindrome using a while loop.
+// let n=85476, digit,rev=0
+// temp=n
+// while(n>0){
+//    console.log("The number is :",n) 
+//    digit=n%10
+//    console.log(digit)
+//    n= Math.floor(n/10)
+//    console.log("The number  after module is :",n) 
+//    rev=rev*10+digit
+//    console.log("rev digit :",rev) 
+// }
+// if(temp==rev)
+// {
+//     console.log("Number Palidrom Hai")
+// }
+// else{
+//     console.log("Number Palidrom nahi Hai")
+
+// }
+// 9.Print numbers from 1 to 100 but skip multiples of 5 using a while loop.
+// let i=1
+// while(i<=100){
+//     if(i%5!=0){
+//         console.log(i)
+//     }
+//     i++
+// }
+// 10.Print the countdown for a timer (e.g., 10 to 0) using a while loop.
+// let n=10
+// while(n>=0)
+// {
+//     console.log(n)
+//     n--
+// }
